2a3d0d6866a09bd928396f4829854974719d04c7
Jason Paek <adventuredonut343@gmail.com>
2024-08-17 01:35:11 -0400

    Planning and commenting, create git ignore for key

---
+++ CurEx.cpp
@@ -0,0 +1,15 @@
+/*Currency Exchange Tool using ExchangeRate-API by Jason Paek*/
+#include <iostream>
+#include <string>
+
+// Callback function 
+
+// fetch exchange rate from API
+
+// Main function logic: api key, base currency, target currency, amount to convert. call function, retrieval, calculate, display, and handle errors
+
+// Error Handling: APi, HTTP, JSON parse etc
+
+// cleanup/resource management
+
+
